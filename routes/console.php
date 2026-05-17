1ca44c2c2258717447582ab57dde0f33180a70f6
ppdra <pepedra03@gmail.com>
2026-05-17 11:25:15 +0100

    chenge scheduler time

--- routes/console.php
+++ routes/console.php
@@ -47,4 +47,4 @@ app(Schedule::class)->command('crazy:insert-teams')->twiceDaily(0, 12);
 
 app(Schedule::class)->command('crazy:insert-matches')->dailyAt('00:00');
 
-app(Schedule::class)->command('crazy:insert-matches-results')->everyFifteenMinutes();
+app(Schedule::class)->command('crazy:insert-matches-results')->everyTwoHours();
