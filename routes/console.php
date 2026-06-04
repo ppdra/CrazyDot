e29878053d21e74f65457c6a9ed7d540156edb83
ppdra <pepedra03@gmail.com>
2026-06-04 11:12:56 +0100

    add sheduler para bkp to excel

--- routes/console.php
+++ routes/console.php
@@ -48,3 +48,5 @@ app(Schedule::class)->command('crazy:insert-teams')->twiceDaily(0, 12);
 app(Schedule::class)->command('crazy:insert-matches')->dailyAt('00:00');
 
 app(Schedule::class)->command('crazy:insert-matches-results')->everyTwoHours();
+
+app(Schedule::class)->command('crazy:backup-to-xlsx')->twiceDaily(3,15);
